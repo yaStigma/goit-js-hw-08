69613b436c7a10a11c93804360031db32101ac08
Tanya Yenakieva <yastigma@gmail.com>
2023-11-13 23:22:42 +0200

    task 2
    to be continued ...

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+// import Player from '@vimeo/player';
 
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe, {
+//     id: 19231868,
+//     width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
+
+// const onPlay = function(data) {
+//     // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
